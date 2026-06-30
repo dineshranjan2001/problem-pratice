472e7f2de4a0f50800fca4314b0c310fdcd8f59a
Dinesh Ranjan Biswal <124261545+dineshranjan2001@users.noreply.github.com>
2026-06-30 10:04:34 +0000

    find largest number.

---
+++ find_largest_number.js
@@ -0,0 +1,24 @@
+// find the largest number from the array
+// here 10 is the highest number.
+const arr = [4, 2, 10, 7, 5];
+
+// using for loop
+let highestNumber=0;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]>highestNumber){
+        highestNumber=arr[i];
+    }
+}
+console.log(highestNumber)
+
+// using reduce function
+const highestNumber1=arr.reduce((max,ele)=>{
+    return ele>max?ele:max
+});
+console.log(highestNumber1);
+
+// using Math.max function.
+const highestNumber2=Math.max(...arr);
+console.log(highestNumber2);
+
+
