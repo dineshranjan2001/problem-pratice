2dd6e47cffef694c1f31313166080a716a0b4d3e
Dinesh Ranjan Biswal <124261545+dineshranjan2001@users.noreply.github.com>
2026-07-06 08:20:40 +0000

    add the 2nd approach of two pointer.

--- remove_duplicates_char.js
+++ remove_duplicates_char.js
@@ -11,6 +11,7 @@ charArray.forEach((char)=>{
 console.log(uniqueCharArray.join(''))
 
 //2nd way
+//1st way to remove duplicate chars from a string using two pointer approach
 const chars=duplicateString.split('');
 const check={};
 let writeIndex=0;
@@ -24,4 +25,16 @@ for(let readIndex=0;readIndex<=chars.length;readIndex++){
 }
 
 chars.length=writeIndex;
+console.log(chars.join(''));
+
+//2nd way to remove duplicate chars from a string using two pointer approach
+const chars1=duplicateString.split('');
+let i=0;
+for(let j=1;j<chars1.length;j++){
+    if(chars1[j]!==chars1[i]){
+        i++;
+        chars1[i]=chars1[j];
+    }
+}
+chars1.length=i+1;
 console.log(chars.join(''));
