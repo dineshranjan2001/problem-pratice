67823d608a9e33eb494f8124d83c23034761b17d
Dinesh Ranjan Biswal <124261545+dineshranjan2001@users.noreply.github.com>
2026-07-06 17:11:50 +0000

    add the logic of group by property.

--- group_by_property.js
+++ group_by_property.js
@@ -10,3 +10,28 @@ const data = [
     30:[...]
     }
 */
+
+const newObject={};
+for(let arr of data){
+  if(!newObject[arr.age]){
+    newObject[arr.age]=[]
+  }
+}
+
+for(let arr of data){
+  newObject[arr.age].push(arr);
+}
+console.log(newObject)
+
+
+// using reduce function
+const result=data.reduce((acc,user)=>{
+  if(!acc[user.age]){
+    acc[user.age]=[];
+  }
+
+  acc[user.age].push(user);
+  return acc;
+},{});
+
+console.log(result);
