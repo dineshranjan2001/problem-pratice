108915b37fb825fc952e6b68a4579bafd062e829
Dinesh Ranjan Biswal <124261545+dineshranjan2001@users.noreply.github.com>
2026-07-06 11:20:12 +0000

    add another way.

--- first_non_repeating_char.js
+++ first_non_repeating_char.js
@@ -9,3 +9,19 @@ for(let char of string){
     }
 }
 console.log(missingChar);
+
+// in a optimized way
+
+const freq={};
+for(let char of string){
+    freq[char]=(freq[char] || 0) +1
+}
+
+let nonRepeatChar='';
+for(let char of string){
+    if(freq[char]===1){
+        nonRepeatChar=char;
+        break;
+    }
+}
+console.log(nonRepeatChar);
