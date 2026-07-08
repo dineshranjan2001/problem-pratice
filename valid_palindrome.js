718564cb962bfea7da6c6735a0b2378c1f7836a1
Dinesh Ranjan Biswal <124261545+dineshranjan2001@users.noreply.github.com>
2026-07-08 11:19:58 +0000

    add 2nd way in the valid_palindrome.

--- valid_palindrome.js
+++ valid_palindrome.js
@@ -1,12 +1,35 @@
 //Valid Palindrome: Check if a string reads the same forward and backward after removing non-alphanumeric characters.
 
-const nonAlphaNumbericString = 'a$a*c(c)a#b';
+const nonAlphaNumbericString = 'a$a*c(c)a#a';
 
 const checkValidPalindrome = (string) => {
     const regex = /[^a-zA-Z0-9 ]/g;
-    const validString = string.replace(regex,'').trim();
-    return (validString===validString.split('').reverse().join(''))? true : false;
-    
+    const validString = string.replace(regex, '').trim();
+    return (validString === validString.split('').reverse().join('')) ? true : false;
+
+}
+console.log(checkValidPalindrome(nonAlphaNumbericString));
+
+const checkValidPalindrome1 = (string) => {
+    const regex = /[^a-zA-Z0-9 ]/g;
+    let i = 0;
+    let j = string.length - 1;
+    while (i < j) {
+        while (i < j && regex.test(string[i])) {
+            i++;
+        }
+
+        while (i < j && regex.test(string[j])) {
+            j--
+        }
+
+        if (string[i].toLowerCase() !== string[j].toLowerCase()) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 }
 
-console.log(checkValidPalindrome(nonAlphaNumbericString));
+console.log(checkValidPalindrome1(nonAlphaNumbericString));
