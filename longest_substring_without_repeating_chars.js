1ecaeb7498bb7d3df3da9e9f22e0c0f0267fa547
Dinesh Ranjan Biswal <124261545+dineshranjan2001@users.noreply.github.com>
2026-07-09 13:22:17 +0000

    longest_substring_without_repeating_char.

---
+++ longest_substring_without_repeating_chars.js
@@ -0,0 +1,18 @@
+/*
+    abcabcbb
+    output-3
+    
+
+*/
+
+
+const longestLengthofString=(str)=>{
+    let i=0;
+    let maxLength=0;
+    const set=new Set();
+    for(let char of str){
+        if(set.has(str[char])){
+
+        }
+    }
+}
