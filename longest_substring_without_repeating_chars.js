21915d18105ef015a98e023f283a2a3785917c75
Dinesh Ranjan Biswal <124261545+dineshranjan2001@users.noreply.github.com>
2026-07-11 14:29:11 +0000

    commit the solution of longest substring without repeating characters.

--- longest_substring_without_repeating_chars.js
+++ longest_substring_without_repeating_chars.js
@@ -5,14 +5,20 @@
 
 */
 
-
 const longestLengthofString=(str)=>{
     let i=0;
     let maxLength=0;
     const set=new Set();
-    for(let char of str){
-        if(set.has(str[char])){
-
+    for(let j=0;j<=str.length;j++){
+        while(set.has(str[j])){
+            set.delete(str[i]);
+            i++;
         }
+        set.add(str[j]);
+        maxLength=Math.max(maxLength,j-i+1);
+        
     }
-}
+    return maxLength;
+}    
+
+console.log("longest length of string ",longestLengthofString('abcabcbb'));
